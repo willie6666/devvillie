b8c47b29ce3f4c62912d7ee457bfaa4c39343882
willie6666 <willie35212736@gmail.com>
2026-03-22 00:41:48 +0800

    Add robots.txt to manage web crawler access and specify sitemap location & update Acout section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://devvillie.me',
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: cloudflare(),
+  integrations: [sitemap()],
 });
