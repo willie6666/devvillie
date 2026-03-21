d27e73b849bfb60b41222d5e8d865c88ecf3edf4
willie6666 <willie35212736@gmail.com>
2026-03-21 17:23:27 +0800

    chore: update package.json and tsconfig.json for Cloudflare integration
    - Added `@astrojs/cloudflare` and `wrangler` dependencies in package.json.
    - Introduced a new script `generate-types` for generating types with Wrangler.
    - Updated tsconfig.json to include `worker-configuration.d.ts` for type definitions.
    - Created a new wrangler.jsonc configuration file for Cloudflare deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
